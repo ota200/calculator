23903d14b3c4071d8953daacc4a16d9a952d4550
O.T.A <79542378+ota200@users.noreply.github.com>
2021-07-03 17:43:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let operator = /
+let num_1 = 0
+let num_2 = 0
+
+let words = document.getElementById("words");
+
+function enter(){
+	let words_value = words.value;
+	console.log(words_value);
+	console.log(words_value.split('');
+};
